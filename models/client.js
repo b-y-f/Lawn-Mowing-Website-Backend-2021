50a82ba6773eecf7881337f9553608465acb0ad1
yb55 <yifanbao522@gmail.com>
2021-08-13 03:24:40 +1200

    build client schema

---
+++ models/client.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const clientSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  name: String,
+  address: String,
+  phone: String,
+  email: String,
+  passwordHash: String,
+  quotes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Quote'
+    }
+  ]
+})
+
+clientSchema.plugin(uniqueValidator)
+
+clientSchema.set('toJson', {
+  transform: (doc, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString()
+    delete returnedObj._id
+    delete returnedObj.__v
+    delete returnedObj.passwordHash
+  }
+})
+
+const Client = mongoose.model('Client', clientSchema)
+
+module.exports = Client
