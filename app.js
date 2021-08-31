9125f5f59162e72cd306b5d890a81a92ea017278
Yifan Bao <bbbbb8bbbbb@gmail.com>
2021-08-31 23:36:47 +1200

    enable req logger better debug

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-const routeRouter = require('./controllers/quotes')
+const qouteRouter = require('./controllers/quotes')
 const clientsRouter = require('./controllers/clients')
 const loginRouter = require('./controllers/login')
 const guestsRouter = require('./controllers/guests')
 const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
+app.use(middleware.requestLogger)
 
 mongoose.connect(config.MONGODB_URI, {
   useNewUrlParser: true,
@@ -21,13 +23,13 @@ mongoose.connect(config.MONGODB_URI, {
   useCreateIndex: true
 })
   .then(result => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch(err => {
-    console.log('fail to connect MongoDB', err.message)
+    logger.info('fail to connect MongoDB', err.message)
   })
 
-app.use('/api/quotes', routeRouter)
+app.use('/api/quotes', qouteRouter)
 app.use('/api/clients', clientsRouter)
 app.use('/api/guests', guestsRouter)
 app.use('/api/login', loginRouter)
